06de1415187f11634d77ab92f0b5ac21cd14fb38
Veronika <tokareva_v@inbox.ru>
2022-10-14 09:12:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿////  Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.Clear()
+Console.WriteLine("Вторая цифра трехзначного числа.");
+Console.WriteLine("Трехзначное число:");
+int x = int.Parse(Console.ReadLine()!)
+int result = 0;
+int x1 = x/10;
+if (x1>= 10 $$x1 < 100)
+{
+    result = (x % 100)/10;
+    Console.WriteLine($"Вторая цифра числа {x} > {result}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное.")
